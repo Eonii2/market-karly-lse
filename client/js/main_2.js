2e5eb1cd347fdcd51e1332397c6276b3e666cdfc
Eonii2 <eoniioff@gmailcom>
2023-02-08 23:44:48 +0900

    initial commit

--- client/js/main_2.js
+++ client/js/main_2.js
@@ -9,10 +9,10 @@ const swiperB = new Swiper(".big-banner-swiper", {
   loop: true, // 무한루프로 걸어서 맨 앞으로 후루룩 돌아오는 걸 방지
   parallax: true, // 시차를 이용한 효과
   speed: 1000,
-  pagination: {
-    el: ".swiper-pagination", // 버튼을 담을 태그 설정
-    type: "fraction", // 기본 슬라이더를 fraction으로 변경
-  },
+  // pagination: {
+  //   el: ".swiper-pagination", // 버튼을 담을 태그 설정
+  //   type: "fraction", // 기본 슬라이더를 fraction으로 변경
+  // },
   navigation: {
     // 버튼
     nextEl: ".big-banner-next",
